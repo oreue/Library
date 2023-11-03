89efa2691e5ce0c306cf9b2f8e920b4778848cf4
Patryk <patryk.surowiec@hotmail.com>
2023-11-03 22:23:11 +0100

    First Commit

---
+++ src/main/java/app/Library.java
@@ -0,0 +1,28 @@
+package app;
+
+import io.DataReader;
+import model.Book;
+
+public class Library {
+
+    public static void main(String[] args) {
+
+        final String appName = "Biblioteka v0.8";
+
+        Book[] books = new Book[1000];
+        DataReader dataReader = new DataReader();
+
+
+        System.out.println(appName);
+        System.out.println("Wprowadź nową książkę:");
+        books[0] = dataReader.readAndCreateBook();
+        books[1] = dataReader.readAndCreateBook();
+        dataReader.close();
+
+        books[0].printInfo();
+        books[1].printInfo();
+        System.out.println("System może przechowywać do " + books.length + " książek");
+
+    }
+
+}
